001a3a95c523de77ea1cf72963c90f476cfc1ef4
Arnapurna Satapathy <34472128+s-arnapurna@users.noreply.github.com>
2025-05-25 02:45:26 +0000

    Deploy React app to root

--- src/components/EmailLink.js
+++ src/components/EmailLink.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 export default function EmailLink() {
   return (
-    <a href="mailto:you@domain.com" className="email-link">
-      you@domain.com
+    <a href="mailto:msarnapurnasatapathy@gmail.com" className="email-link">
     </a>
   );
 }
